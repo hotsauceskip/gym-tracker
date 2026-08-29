7b7ae7bbaf9dc23bca325fdb238318c639129b8d
hotsauceskip <hotsauceskip@gmail.com>
2026-08-29 17:08:33 +0300

    bump cache version

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Простой offline-first сервис-воркер: кэшируем весь app shell при установке.
-const CACHE_NAME = "gym-tracker-v1";
+const CACHE_NAME = "gym-tracker-v2";
 const ASSETS = [
   "./",
   "./index.html",
