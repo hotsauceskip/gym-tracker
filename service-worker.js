2ee8019ddae43475d50b262b4a0bbec0d3f1122e
hotsauceskip <hotsauceskip@gmail.com>
2026-08-29 17:22:35 +0300

    fix: persistent rest timer - service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Простой offline-first сервис-воркер: кэшируем весь app shell при установке.
-const CACHE_NAME = "gym-tracker-v3";
+const CACHE_NAME = "gym-tracker-v4";
 const ASSETS = [
   "./",
   "./index.html",
