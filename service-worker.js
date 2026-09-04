c235557fb34b5b6513499976ad65b64d9adafad0
hotsauceskip <hotsauceskip@gmail.com>
2026-09-04 22:16:26 +0300

    feat: RIR only on last set + duplicate-set button - service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Простой offline-first сервис-воркер: кэшируем весь app shell при установке.
-const CACHE_NAME = "gym-tracker-v8";
+const CACHE_NAME = "gym-tracker-v9";
 const ASSETS = [
   "./",
   "./index.html",
